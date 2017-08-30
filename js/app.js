c8f6eab75dcfc82e0c5b37a229e874ab24eed3d0
Huu Tinh Nguyen Pham <huutinh@berkeley.edu>
2017-08-29 22:40:59 -0700

    we have movement!

--- js/app.js
+++ js/app.js
@@ -1,10 +1,13 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(row, speed) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
+    this.x = 0;
+    this.y = row * 83;
+    this.speed = speed;
     this.sprite = 'images/enemy-bug.png';
 };
 
@@ -14,6 +17,7 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x = this.x + this.speed*dt;
 };
 
 // Draw the enemy on the screen, required method for game
@@ -24,12 +28,60 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function(row, col) {
+  this.col = col;
+  this.row = row;
+  this.sprite = 'images/char-boy.png';
+};
+
+Player.prototype.update = function(dt) {
+    // You should multiply any movement by the dt parameter
+    // which will ensure the game runs at the same speed for
+    // all computers.
+};
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.col * 101, this.row * 83 - 12);
+};
+
+Player.prototype.handleInput = function(keyInput) {
+  switch (keyInput) {
+    case 'left':
+      if (this.col > 0) this.col -= 1;
+      break;
+    case 'right':
+      if (this.col < 4) this.col += 1;
+      break;
+    case 'up':
+      if (this.row > 0) this.row -= 1;
+      break;
+    case 'down':
+      if (this.row < 5) this.row += 1;
+      break;
+  };
+};
+
+var createEnemy = function (DIFFICULTY) {
+  var random = Math.random();
+  return new Enemy(Math.ceil(3*random), Math.ceil(random*DIFFICULTY)*200);
+}
+
+var createPlayer = function() {
+  var random = Math.random();
+  return new Player(5, Math.round(random*5));
+}
 
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+var DIFFICULTY = 1;
+allEnemies = [];
+player = createPlayer();
 
+for (i = 0; i < 3*DIFFICULTY; i++) {
+  allEnemies.push(createEnemy(DIFFICULTY));
+}
 
 
 // This listens for key presses and sends the keys to your
